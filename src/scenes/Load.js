c4569fcf7df2951a21a2c3223f124b4aa7013fa8
Anika <anmahaja@ucsc.edu>
2022-11-28 20:29:52 -0800

    added updated ET sprite

--- src/scenes/Load.js
+++ src/scenes/Load.js
@@ -6,8 +6,9 @@ class Load extends Phaser.Scene{
     preload(){
         //this.load.image('ET', './assets/ET.png');
         //this.load.spritesheet();
-        this.load.spritesheet('ET', './assets/ET_spritesheet.png', {frameWidth: 16, frameHeight: 22, endFrame: 6, spacing: 1});
-        this.load.spritesheet('background', './assets/background_spritesheet.png', {frameWidth: 320, frameHeight: 210, endFrame: 8, margin: 1, spacing: 3});
+        // this.load.spritesheet('ET', './assets/ET_spritesheet.png', {frameWidth: 16, frameHeight: 22, endFrame: 6, spacing: 1});
+        this.load.spritesheet('background', './assets/background_spritesheet.png', { frameWidth: 320, frameHeight: 210, endFrame: 8, margin: 1, spacing: 3 });
+        this.load.spritesheet('ET', './assets/ET_sprites.png', {frameWidth: 43, frameHeight: 63, endFrame: 6, spacing: 1});
         //this.load.audio();
     }
 
