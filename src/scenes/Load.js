c428c8544c0b8fc042b42ca053d8234d6b6df0a8
nimtiaz-ucsc <nimtiaz@ucsc.edu>
2022-12-01 18:05:05 -0800

    Added menu screen assets

--- src/scenes/Load.js
+++ src/scenes/Load.js
@@ -4,6 +4,19 @@ class Load extends Phaser.Scene{
     }
 
     preload(){
+        //menu assets
+        this.load.image('menu_bg', './assets/menu/background.png');
+        this.load.image('glow1', './assets/menu/glow1.png');
+        this.load.image('glow2', './assets/menu/glow2.png');
+        this.load.image('glow3', './assets/menu/glow3.png');
+        this.load.image('moon', './assets/menu/moon.png');
+        this.load.image('text1', './assets/menu/text1.png');
+        this.load.image('text2', './assets/menu/text2.png');
+        this.load.image('tree1', './assets/menu/tree1.png');
+        this.load.image('tree2', './assets/menu/tree2.png');
+        this.load.spritesheet('menu_ET', './assets/menu/et_sheet.png', {frameWidth: 480, frameHeight: 270, endFrame: 3});
+
+
         this.load.spritesheet('background', './assets/background_spritesheet.png', { frameWidth: 320, frameHeight: 210, endFrame: 8, margin: 1, spacing: 3 });
         this.load.spritesheet('ET', './assets/ET_sprites_002.png', { frameWidth: 29, frameHeight: 61, endFrame: 6, spacing: 1 });
         this.load.spritesheet('FBI', './assets/fbi_sprite.png', { frameWidth: 42, frameHeight: 64, endFrame: 3 });
